7a9b607f3458b9fe3b87b840b907aa0725327a25
dpasca <devnull@localhost>
2009-03-26 17:52:50 +0000

    - Adding missing light source header

---
+++ RI_System/include/RI_LightSource.h
@@ -0,0 +1,41 @@
+//==================================================================
+/// RI_LightSource
+///
+/// Created by Davide Pasca - 2009/3/25
+/// See the file "license.txt" that comes with this project for
+/// copyright info. 
+//==================================================================
+
+#include "RI_Base.h"
+
+//==================================================================
+namespace RI
+{
+
+// LightSource "distantlight" 2 "intensity" [1] "lightcolor" [0.5 0.2 0.0] "from" [-1000 500 1000] "to" [0 0 0]
+//==================================================================
+/// LightSource
+//==================================================================
+class LightSource
+{
+public:
+	enum Type
+	{
+		TYPE_DISTANT,
+		TYPE_AMBIENT,
+	};
+public:
+
+	Type	mType;
+	u_int	mID;
+	float	mIntesity;
+	Color	mColor;
+	Point3	mFromPos;
+	Point3	mToPos;
+
+	LightSource();
+};
+
+
+//==================================================================
+}
