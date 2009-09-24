7bb1bf184a31f0ee0a05ec4ed5675aa54dfd4aad
dpasca <devnull@localhost>
2009-09-24 16:15:36 +0000

    - Added additional spaces tests (which, of course, don't compile 8)

---
+++ DistribSrc/base/TestScenes/SpacesTests/rend_tests_aqsis.bat
@@ -0,0 +1,15 @@
+aqsis SpacesTestCamera.rib
+move ri.pic SpacesTestCamera.pic
+
+aqsis SpacesTestObject.rib
+move ri.pic SpacesTestObject.pic
+
+aqsis SpacesTestRaster.rib
+move ri.pic SpacesTestRaster.pic
+
+aqsis SpacesTestScreen.rib
+move ri.pic SpacesTestScreen.pic
+
+aqsis SpacesTestWorld.rib
+move ri.pic SpacesTestWorld.pic
+
