78e90c739538511d93f14eaf938297fd9458b3f7
dpasca <devnull@localhost>
2010-01-04 18:49:25 +0000

    - Improved documentation and general distribution - Added Killeroo for the package distribution as well

--- DistribSrc/pc/MakeTests.bat
+++ DistribSrc/pc/MakeTests.bat
@@ -5,6 +5,7 @@ call render
 popd
 
 for /f %%a IN ('dir /b TestScenes\*.rib') do RibRenderFile.exe clientmode TestScenes\%%a TestsOutput\%%~na.jpg
+for /f %%a IN ('dir /b TestScenes\KillerooNURBS\*.rib') do RibRenderFile.exe clientmode TestScenes\KillerooNURBS\%%a TestsOutput\%%~na.jpg
 for /f %%a IN ('dir /b TestScenes\SpacesTests\*.rib') do RibRenderFile.exe clientmode TestScenes\SpacesTests\%%a TestsOutput\%%~na.jpg
 for /f %%a IN ('dir /b TestScenes\LightsTests\*.rib') do RibRenderFile.exe clientmode TestScenes\LightsTests\%%a TestsOutput\%%~na.jpg
 for /f %%a IN ('dir /b TestScenes\Sponza\sponza.rib') do RibRenderFile.exe clientmode TestScenes\Sponza\%%a TestsOutput\%%~na.jpg
