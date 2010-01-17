f34cabf3a0db5a54881f36759fd12e90db483d3a
dpasca <devnull@localhost>
2010-01-17 16:00:11 +0000

    - This replaces RibRenderFile (doing step by step because of SVN problems)

---
+++ RibRender/RibRender.h
@@ -0,0 +1,35 @@
+//==================================================================
+/// RibRender.h
+///
+/// Created by Davide Pasca - 2009/4/12
+/// See the file "license.txt" that comes with this project for
+/// copyright info. 
+//==================================================================
+
+#ifndef RIBRENDERFILE_H
+#define RIBRENDERFILE_H
+
+#include <stdio.h>
+#include <stdexcept>
+
+#if defined(WIN32)
+#include <direct.h>
+#else
+//#include <direct.h>
+#endif
+
+#include "RibRenderLib/include/RibRenderLib.h"
+#include "DSystem/include/DUtils.h"
+#include "DSystem/include/DUtils_Files.h"
+#include "DSystem/include/DNetwork.h"
+#include "DSystem/include/DCriticalSection.h"
+#include "RenderOutputFile.h"
+#include "RibRender.h"
+
+void PrintUsage( int argc, char **argv );
+
+int ServerMain( int argc, char **argv );
+int ClientMain( int argc, char **argv );
+
+
+#endif
