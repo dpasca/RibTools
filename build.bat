d25eb2026cfb563459b705c34e49fb3131281173
dpasca <devnull@localhost>
2010-01-18 19:35:56 +0000

    - Fixed build to include the PDF files in the docs folder into Distrib

--- build.bat
+++ build.bat
@@ -10,4 +10,5 @@ mkdir Distrib\docs
 mkdir Distrib\docs\docs_content
 xcopy /E /Y docs\*.txt Distrib\docs\
 xcopy /E /Y docs\*.html Distrib\docs\
+xcopy /E /Y docs\*.pdf Distrib\docs\
 xcopy /E /Y docs\docs_content Distrib\docs\docs_content
